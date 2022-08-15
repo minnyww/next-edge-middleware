ffa71d5a2d42ecfb191cf2f7f5d9793108211b9f
minnyww <minrrverse@gmail.com>
2022-08-15 23:50:00 +0700

    [edit] tp last

--- middleware.js
+++ middleware.js
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 
 export async function middleware(req) {
   const { nextUrl: url, geo } = req
+  console.log('geo ---> ', geo)
+  console.log('--->', geo.country)
   url.searchParams.set('country', geo.country)
   return NextResponse.rewrite(url)
 }
